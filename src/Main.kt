5db8326a99773c2bf5735c461764085d63daa469
Ruben Leiva <rubenlrd@gmail.com>
2024-03-05 20:05:33 -0300

    se actualiza el archivo

---
+++ src/Main.kt
@@ -0,0 +1,11 @@
+package src
+
+fun main(){
+    val num1:Int = 5
+    val num2:Int = 9
+    var resultado:Int = num1 + num2
+    println("El resultado de la suma de $num1 + $num2 es: $resultado")
+
+    resultado = num1 * num2
+    println("El resultado del producto de $num1 * $num2 es: $resultado")
+}
